f6a432b6c3e8df8b962840f2b8fed09ec2906aaf
Monosune <thiagoassisdecarvalho@gmail.com>
2025-06-09 18:44:19 -0300

    trabalhando com as listas

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,26 @@
 import "./css/List.scss";
+import { Apple } from "lucide-react";
 
 function List() {
   return (
     <div className="list-main">
       <div>
-        <h1>Lista</h1>
+        <div className="list-item">
+          <Apple />
+          <h2>Lorem ipsum</h2>
+          <h3>R$</h3>
+          <input type="checkbox" name="itemcheck1" id="itemcheck1" />
+        </div>
+        <div className="list-item">
+          <Apple />
+          <h2>Lorem ipsum</h2>
+          <h3>R$</h3>
+          <input type="checkbox" name="itemcheck2" id="itemcheck2" />
+        </div>
+        <div className="bottom-text-value">
+          <h3>N° de itens</h3>
+          <h3>(Valor final)</h3>
+        </div>
       </div>
     </div>
   );
