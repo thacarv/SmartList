f68356e9b15fb11f00dd59720b23b6637ad50e95
Monosune <thiagoassisdecarvalho@gmail.com>
2025-06-16 16:56:20 -0300

    0.6.0
    Refactoring the code.
    Created a new "Body.jsx" component to store List and TotalValue components and some functions related to these 2 components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import MainMenu from "./components/MainMenu.jsx";
-import List from "./components/List.jsx";
-import TotalValue from "./components/TotalValue.jsx";
 
 import { useState } from "react";
 import { v4 } from "uuid";
@@ -10,7 +8,6 @@ import Body from "./components/Body.jsx";
 function App() {
   const [itemList, setItemList] = useState([]);
   const [finalPrice, setFinalPrice] = useState(0);
-  const [currentValue, setCurrentValue] = useState(0);
 
   function onAddItemSubmit(item, price) {
     const newItem = {
@@ -27,79 +24,14 @@ function App() {
     setFinalPrice(newPrice);
   }
 
-  function onItemClick(itemID) {
-    const updateItem = itemList.map((item) => {
-      if (item.id === itemID) {
-        checkCurrentValue(item);
-        return { ...item, isChecked: !item.isChecked };
-      }
-
-      return item;
-    });
-    setItemList(updateItem);
-  }
-
-  function checkCurrentValue(item) {
-    const itemFullPrice = item.price * item.count;
-    const newValue = item.isChecked
-      ? currentValue - itemFullPrice
-      : currentValue + itemFullPrice;
-    setCurrentValue(newValue);
-  }
-
-  function checkCurrentFinalPrice(item, type) {
-    const price = item.price * item.count;
-    console.log(price);
-    const itemFullPrice = type
-      ? finalPrice + price
-      : finalPrice - price / item.count;
-
-    setFinalPrice(itemFullPrice);
-  }
-
-  function onExcludeClick(itemID) {
-    const updateItem = itemList
-      .map((item) => {
-        if (item.id === itemID) {
-          checkCurrentFinalPrice(item, false);
-          if (item.count === 1) {
-            return { ...item, toExclude: true };
-          }
-          const result = item.count - 1;
-          return { ...item, count: result };
-        }
-        return item;
-      })
-      .filter((item) => item.toExclude === false);
-
-    setItemList(updateItem);
-  }
-
-  function onPlusClick(itemID) {
-    const updateItem = itemList.map((item) => {
-      if (item.id === itemID) {
-        const result = item.count + 1;
-        checkCurrentFinalPrice(item, true);
-        return { ...item, count: result };
-      }
-      return item;
-    });
-    setItemList(updateItem);
-  }
-
   return (
     <>
       <MainMenu onAddItemSubmit={onAddItemSubmit} itemList={itemList} />
-      <List
+      <Body
         itemList={itemList}
-        onItemClick={onItemClick}
-        onExcludeClick={onExcludeClick}
-        onPlusClick={onPlusClick}
-      />
-      <TotalValue
-        itemList={itemList}
+        setItemList={setItemList}
         finalPrice={finalPrice}
-        currentValue={currentValue}
+        setFinalPrice={setFinalPrice}
       />
     </>
   );
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,82 @@
-import { List } from "lucide-react";
-import TotalValue from "./TotalValue";
+import { useState } from "react";
 
-function Body() {
+import List from "./List.jsx";
+import TotalValue from "./TotalValue.jsx";
+
+function Body({ itemList, setItemList, finalPrice, setFinalPrice }) {
+  const [currentValue, setCurrentValue] = useState(0);
+
+  function onItemClick(itemID) {
+    const updateItem = itemList.map((item) => {
+      if (item.id === itemID) {
+        checkCurrentValue(item);
+        return { ...item, isChecked: !item.isChecked };
+      }
+
+      return item;
+    });
+    setItemList(updateItem);
+  }
+
+  function checkCurrentValue(item) {
+    const itemFullPrice = item.price * item.count;
+    const newValue = item.isChecked
+      ? currentValue - itemFullPrice
+      : currentValue + itemFullPrice;
+    setCurrentValue(newValue);
+  }
+
+  function checkCurrentFinalPrice(item, type) {
+    const price = item.price * item.count;
+    const itemFullPrice = type
+      ? finalPrice + price
+      : finalPrice - price / item.count;
+
+    setFinalPrice(itemFullPrice);
+  }
+
+  function onExcludeClick(itemID) {
+    const updateItem = itemList
+      .map((item) => {
+        if (item.id === itemID) {
+          checkCurrentFinalPrice(item, false);
+          if (item.count === 1) {
+            return { ...item, toExclude: true };
+          }
+          const result = item.count - 1;
+          return { ...item, count: result };
+        }
+        return item;
+      })
+      .filter((item) => item.toExclude === false);
+
+    setItemList(updateItem);
+  }
+
+  function onPlusClick(itemID) {
+    const updateItem = itemList.map((item) => {
+      if (item.id === itemID) {
+        const result = item.count + 1;
+        checkCurrentFinalPrice(item, true);
+        return { ...item, count: result };
+      }
+      return item;
+    });
+    setItemList(updateItem);
+  }
   return (
     <div>
-      <List />
-      <TotalValue />
+      <List
+        itemList={itemList}
+        onItemClick={onItemClick}
+        onExcludeClick={onExcludeClick}
+        onPlusClick={onPlusClick}
+      />
+      <TotalValue
+        itemList={itemList}
+        finalPrice={finalPrice}
+        currentValue={currentValue}
+      />
     </div>
   );
 }
