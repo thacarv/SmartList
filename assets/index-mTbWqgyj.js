1c160cb04bf54588dfe3bd0b644e41a7118e316c
Monosune <thiagoassisdecarvalho@gmail.com>
2025-07-12 22:41:46 -0300

    Update index-mTbWqgyj.js

--- assets/index-mTbWqgyj.js
+++ assets/index-mTbWqgyj.js
@@ -12273,7 +12273,7 @@ function Rm(i) {
           className: "list-name",
           children: [
             $.jsx("img", {
-              src: "src/assets/tartarugas-em-compras.png",
+              src: "./assets/tartarugas-em-compras.png",
               alt: "",
             }),
             $.jsx("h3", { children: "SmartList" }),
